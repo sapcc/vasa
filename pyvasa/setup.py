139a041688d2f6ac0ed3b87fee301665c0ef9e91
d066961 <hannes.ebelt@sap.com>
2019-06-12 11:30:00 +0200

    update setup.py

--- pyvasa/setup.py
+++ pyvasa/setup.py
@@ -5,16 +5,16 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='pyvasa',
-    version='1.3',
+    version='1.0',
     author='Hannes Ebelt',
     author_email='hannes.ebelt@sap.com',
     description='Python package for NetApp VASA Appliance',
     url='https://github.com/sapcc/vasa/pyvasa',
     packages=find_packages(),
     install_requires=[
-        'packaging==16.8',
-        'requests==2.18.1',
-        'urllib3==1.21.1'
+        'packaging>=16.8',
+        'requests>=2.18.1',
+        'urllib3>=1.21.1'
         ],
     classifiers=["Programming Language :: Python :: 3"],
     )
